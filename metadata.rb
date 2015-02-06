a9c9a0017598ed59f91ac97e6e0f92bce0bb653b
zadunn <zadunn@gmail.com>
2015-02-06 15:12:31 -0500

    Version Bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer 'devops@optoro.com'
 license 'MIT'
 description 'Installs and configures Zookeeper using exhibitor'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.1'
+version '0.0.2'
 
 depends 'apt'
 depends 'exhibitor'
