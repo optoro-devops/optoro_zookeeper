eeccd647a2c20baa3272cf0ed27b57d22a1927af
Zach Dunn <zdunn@optoro.com>
2015-02-06 15:51:12 -0500

    merge spam

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,7 @@ maintainer 'devops@optoro.com'
 license 'MIT'
 description 'Installs and configures Zookeeper using exhibitor'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-<<<<<<< HEAD
-version '0.0.2'
-=======
 version '0.0.3'
->>>>>>> 6036ae1... Version lockin'
 
 depends 'apt', '>= 2.6.1'
 depends 'exhibitor', '>= 0.4.0'
