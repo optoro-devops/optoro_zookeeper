8fb75556f20e603916e6156718edb9b06f0fb86d
Scott Medefind <smedefind@optoro.com>
2015-12-03 13:51:42 -0500

    Moving zookeeper mirror to apache archive, code cleanup

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,3 +5,4 @@ default['exhibitor']['s3']['secret'] = ''
 default['exhibitor']['cli']['s3config'] = "optoro-exhibitor:#{node.chef_environment}"
 default['exhibitor']['cli']['s3region'] = 'us-east-1'
 node.default!['zookeeper']['group'] = 'zookeeper'
+override['zookeeper']['mirror'] = 'https://archive.apache.org/dist/zookeeper'
